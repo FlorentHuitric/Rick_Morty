5a40a17425eb3a990459a6831e67cd6c93ea2397
FuroRen <florent.huitric@gmail.com>
2022-03-28 14:47:34 +0200

    valid

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ let lastCloud;
 let timeUp = false; //false si pas fini true si fini
 let score = 0;
 
-
+//fonction qui créée un temps d'apparition de la tête aléatoire
 function randomTime(min,max){
     return Math.round(Math.random()*(max-min)+min);
 }
+//fonction qui randomize la tête qui apparaît et qui se relance si une même tête est choisi aléatoirement 2 fois d'affilée
 function randomCloud(clouds){
     const indexCloud = Math.floor(Math.random()*clouds.length);
     const cloudSelect = clouds[indexCloud];
@@ -21,16 +22,38 @@ function randomCloud(clouds){
 
     return cloudSelect;
 }
-function showHead(){
+//fonction qui fait apparaitre les têtes et utilise les résultats des deux fonctions précédentes
+function showHead1(){
     const time = randomTime(600,1000);
     const cloud = randomCloud(clouds);
     cloud.classList.add("up");
     setTimeout(() => {
-        if(!timeUp) showHead();
+        if(!timeUp) showHead1();
         cloud.classList.remove("up");
     } ,time)
-
 }
+
+function showHead2(){
+    const time = randomTime(500,950);
+    const cloud = randomCloud(clouds);
+    cloud.classList.add("up");
+    setTimeout(() => {
+        if(!timeUp) showHead2();
+        cloud.classList.remove("up");
+    } ,time)
+}
+
+function showHead3(){
+    const time = randomTime(300,850);
+    const cloud = randomCloud(clouds);
+    cloud.classList.add("up");
+    setTimeout(() => {
+        if(!timeUp) showHead3();
+        cloud.classList.remove("up");
+    } ,time)
+}
+
+//fonction qui incrémente le score dans le scoreboard
 function playerScore(event){
     if(!event.isTrusted) return;
     score++;
@@ -40,11 +63,12 @@ function playerScore(event){
 
 heads.forEach(head => head.addEventListener("click",playerScore)) ; 
 
-function startGame() {
+//fonction de démarrage du jeu
+function startGame1() {
     scoreBoard.textContent = 0;
     score = 0;
     timeUp = false;
-    showHead();
+    showHead1();
     setTimeout(()=> {
         timeUp = true;
         setTimeout(()=> {
@@ -52,4 +76,83 @@ function startGame() {
         }, 2000);
     } ,10000)
 }
-startGame();
+
+function startGame2() {
+    scoreBoard.textContent = 0;
+    score = 0;
+    timeUp = false;
+    showHead2();
+    setTimeout(()=> {
+        timeUp = true;
+        setTimeout(()=> {
+            scoreBoard.textContent ="end";
+        }, 2000);
+    } ,10000)
+}
+
+function startGame3() {
+    scoreBoard.textContent = 0;
+    score = 0;
+    timeUp = false;
+    showHead3();
+    setTimeout(()=> {
+        timeUp = true;
+        setTimeout(()=> {
+            scoreBoard.textContent ="end";
+        }, 2000);
+    } ,10000)
+}
+
+/* NIVEAUX */
+const speed = 50;
+var i=0;
+var text1 = "NEWBIE";
+
+var j=0;
+var text2 = "INTERMEDIATE";
+
+var k=0;
+var text3 = "HARDCORE";
+
+//fonction de style qui propose les 3 niveaux de difficulté
+function typeWriter1() {
+    if(i<text1.length) {
+        document.getElementById("demo1").innerHTML += text1.charAt(i);
+        i++;
+        setTimeout(typeWriter1,speed);
+    }
+}
+
+function typeWriter2() {
+    if(j<text2.length) {
+        document.getElementById("demo2").innerHTML += text2.charAt(j);
+        j++;
+        setTimeout(typeWriter2,speed);
+    }
+}
+
+function typeWriter3() {
+    if(k<text3.length) {
+        document.getElementById("demo3").innerHTML += text3.charAt(k);
+        k++;
+        setTimeout(typeWriter3,speed);
+    }
+}
+
+//choisir le niveaux de difficulté en cliquant sur Morty
+function myClick() {
+    for(var i=1; i<=3; i++){
+        document.getElementById("demo"+i).addEventListener("click", () => {
+            document.getElementById("demo1").style.display = "none";
+            document.getElementById("demo2").style.display = "none";
+            document.getElementById("demo3").style.display = "none";
+        });
+    }
+}
+
+document.getElementById("morty-play").addEventListener("click", function(){
+    typeWriter1();
+    typeWriter2();
+    typeWriter3();
+    myClick();
+});
